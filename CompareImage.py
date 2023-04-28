87eaee788728c70aa75906af301915adeb5917b2
Alexsandr Liubinskii <a.liubinskii@appshub.family>
2023-04-28 13:36:32 +0400

    CompareImage

---
+++ CompareImage.py
@@ -0,0 +1,40 @@
+import cv2
+
+
+def CalcImageHash(file=str):
+    image = cv2.imread(file)  # Прочитаем картинку
+    resized = cv2.resize(image, (8, 8), interpolation=cv2.INTER_AREA)  # Уменьшим картинку
+    gray_image = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)  # Переведем в черно-белый формат
+    avg = gray_image.mean()  # Среднее значение пикселя
+    ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0)  # Бинаризация по порогу
+
+    # Рассчитаем хэш
+    _hash = ""
+    for x in range(8):
+        for y in range(8):
+            val = threshold_image[x, y]
+            if val == 255:
+                _hash = _hash + "1"
+            else:
+                _hash = _hash + "0"
+
+    return _hash
+
+
+def CompareHash(hash1, hash2):
+    l = len(hash1)
+    i = 0
+    count = 0
+    while i < l:
+        if hash1[i] != hash2[i]:
+            count = count+1
+        i = i+1
+    return count
+
+
+hash1 = CalcImageHash(r'D:\AppsHub\My Bugs\screen_1.png')
+hash2 = CalcImageHash(r"D:\AppsHub\My Bugs\screen_1.png")
+
+print(hash1)
+print(hash2)
+print(CompareHash(hash1, hash2))
